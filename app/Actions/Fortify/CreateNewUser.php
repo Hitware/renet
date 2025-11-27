7b9c77653b50eaf7285f5ca0a9d38cde827f4583
Jhon Agudelo <alexander30j@gmail.com>
2025-11-27 07:13:16 -0500

    Ajustes plataforma

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -37,7 +37,7 @@ class CreateNewUser implements CreatesNewUsers
             'telefono' => $input['telefono'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
-            'role' => $input['role'] ?? 'publico',
+            'role' => $input['role'] ?? 'empresa',
         ]);
     }
 }
